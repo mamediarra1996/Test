b4b773b84ae21b1342bfd65325b0bededb18794c
Cheikhck06 <cheikhck06@gamil.com>
2023-02-07 10:35:26 +0000

    Creation d'une entité User

---
+++ src/main/java/com/example/test/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.test.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+}
